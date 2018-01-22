7cbea43c66d0f5428b10c91cae6b6940eec5359e
0918nobita <nobita.0918@gmail.com>
2018-01-22 19:11:21 +0900

    quicksort, applyTwice, zipWith' 関数を追加した

--- Sample.hs
+++ Sample.hs
@@ -41,3 +41,17 @@ elem' a (x:xs)
     | a == x = True
     | otherwise = a `elem'` xs
 
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
