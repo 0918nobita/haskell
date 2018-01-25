da8f744e2bd40976e96dc8163d651c291b54474a
0918nobita <nobita.0918@gmail.com>
2018-01-26 08:06:11 +0900

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-import Shapes
+import qualified Data.Map as Map
 
 data Person = Person { firstName :: String
     , lastName :: String
@@ -14,3 +14,28 @@ dotProd :: (Num a) => Vector a -> Vector a -> a
 
 vmult :: (Num a) => Vector a -> a -> Vector a
 (Vector i j k) `vmult` m = Vector (i * m) (j * m) (k * m)
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = case Map.lookup lockerNumber map of
+    Nothing -> Left $ "Locker " ++ show lockerNumber ++ " doesn't exist!"
+    Just (state, code) -> if state /= Taken then Right code
+        else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100, (Taken, "ZD39I"))
+    , (101, (Free, "JAH3I"))
+    , (103, (Free, "IQSA9"))
+    , (105, (Free, "QOTSA"))
+    , (109, (Taken, "893JJ"))
+    , (110, (Taken, "99292"))]
+
+infixr 5 :-:
+data List a = Empty | Cons { head :: a, tail :: List a } 
+    | a :-: List a deriving (Show, Read, Eq, Ord)
