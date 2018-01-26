fdc2280e50a882f5f336553cd5186ef00cd42bd3
0918nobita <nobita.0918@gmail.com>
2018-01-27 00:58:17 +0900

    Tree型と^++, singleton, treeInsert 関数を追加した

--- Main.hs
+++ Main.hs
@@ -39,3 +39,20 @@ lockers = Map.fromList
 infixr 5 :-:
 data List a = Empty | Cons { head :: a, tail :: List a } 
     | a :-: List a deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
