1e39984249b152b3a3d48551dc825e2ea19431a4
0918nobita <nobita.0918@gmail.com>
2018-03-04 00:40:54 +0900

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -137,3 +137,13 @@ instance YesNo (Tree a) where
 instance YesNo TrafficLight where
     yesno Red = False
     yesno _ = True
+
+-- class Functor f where
+--     fmap :: (a -> b) -> f a -> f b
+
+-- instance Functor [] where
+--     fmap = map
+
+-- instance Functor Maybe where
+--     fmap f (Just x) = Just (f x)
+--     fmap f Nothing = Nothing
