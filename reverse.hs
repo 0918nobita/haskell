3cea1e4937de2635fcc55bb6c9bad32f8bfbcc2d
0918nobita <nobita.0918@gmail.com>
2018-03-05 16:59:08 +0900

    Control.Monad.when関数のサンプルを追加した

--- reverse.hs
+++ reverse.hs
@@ -1,9 +1,13 @@
+import qualified Control.Monad as Monad
+
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
 
 main :: IO ()
 main = do
     line <- getLine
+    Monad.when (line == "foo") $ do
+        putStrLn "Foooooo!!!!"
     if null line
         then return ()
         else do
