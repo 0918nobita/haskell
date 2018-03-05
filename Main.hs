a82ab05e64df088c34f4c18ec92b8e4e2e90cbf8
0918nobita <nobita.0918@gmail.com>
2018-03-05 16:00:44 +0900

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -147,3 +147,7 @@ instance YesNo TrafficLight where
 -- instance Functor Maybe where
 --     fmap f (Just x) = Just (f x)
 --     fmap f Nothing = Nothing
+
+-- instance Functor (Either a) where
+--     fmap f (Right x) = Right (f x)
+--     fmap f (Left x) = Left x
