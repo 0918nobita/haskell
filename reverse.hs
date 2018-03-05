03b2a18f57d4602da57311395d7d8fbfb8ffbcca
0918nobita <nobita.0918@gmail.com>
2018-03-05 16:20:52 +0900

    Add reverse.hs

---
+++ reverse.hs
@@ -0,0 +1,11 @@
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
+
+main :: IO ()
+main = do
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
