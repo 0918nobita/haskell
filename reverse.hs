cd99c0cb20b5f8be38cd44d70f704bd28288c9cf
0918nobita <nobita.0918@gmail.com>
2018-03-05 19:19:55 +0900

    Control.Monad.forM関数ではなくforM_関数を使用することで、戻り値の型エラーを修正した

--- reverse.hs
+++ reverse.hs
@@ -8,6 +8,8 @@ main = do
     line <- getLine
     Monad.when (line == "foo") $ do
         putStrLn "Foooooo!!!!"
+        Monad.forM_ [1..10] $ \a -> do
+            print a
     if null line
         then return ()
         else do
