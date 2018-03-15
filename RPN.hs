10348978e1ad2299e229f2704877ae4342b2a87c
0918nobita <nobita.0918@gmail.com>
2018-03-15 21:03:06 +0900

    ファンクター則に関するサンプルを追加した

--- RPN.hs
+++ RPN.hs
@@ -10,6 +10,39 @@ solveRPN = head . foldl foldingFunc [] . words
           foldingFunc (x:xs) "ln" = log x : xs
           foldingFunc xs numStr = read numStr : xs
 
+-- <ファンクター則>
+-- 以下のファンクター則 1, 2 を満たす → 適用に関する基本的な振る舞いが関数や他のファンクターと一致することが保証される
+
+-- 1. fmap id = id
+--     id ( a -> a ) でファンクター値を写した場合、ファンクター値が変化してはいけない
+--     > fmap id (Just 3)
+--     Just 3
+
+-- instance Functor Maybe where
+--     fmap f (Just x) = Just (f x)
+--     fmap f Nothing = Nothing
+
+-- 2. fmap (f . g) = fmap f . fmap g 
+--     すべての x に対して fmap (f . g) x = fmap f (fmap g x) が成り立つ
+
+-- fmap (f . g) Nothing = Nothing
+-- fmap f (fmap g Nothing) = fmap f Nothing = Nothing
+
+-- fmap (f . g) (Just x) = Just ((f . g) x) = Just (f (g x)) 
+-- fmap f (fmap g (Just x)) = fmap f (Just (g x)) = Just (f (g x))
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+-- Functor のインスタンスなのにファンクター則を満たせていない例
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter + 1) (f x)
+
+-- > fmap id (CJust 0 "haha")
+-- CJust 1 "haha"
+-- > id (CJust 0 "haha")
+-- CJust 0 "haha"
+
 main :: IO ()
 main = do
     args <- getArgs
