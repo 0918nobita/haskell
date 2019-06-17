2988aaad91e490636f3e3e25b3dedd09f217fce2
0918nobita <nobita.0918@gmail.com>
2019-06-17 20:23:10 +0900

    :construction: 先頭1文字を Char としてパースする item パーサを追加した

--- monadic-parser/src/Lib.hs
+++ monadic-parser/src/Lib.hs
@@ -7,5 +7,10 @@ newtype Parser a = Parser (String -> [(a, String)])
 parse :: Parser a -> String -> [(a, String)]
 parse (Parser p) = p
 
+item :: Parser Char
+item = Parser (\cs -> case cs of
+                  ""      -> []
+                  (c:cs') -> [(c, cs')])
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
